91566e708667a22e3e7f6cad87ce38d22b6fbcf0
Bishwa <bishwakiran725@gmail.com>
2024-01-03 18:33:38 +0545

    Initial Setup React,Vite, Tailwind

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import postcss from 'rollup-plugin-postcss';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    postcss({
+      config: {
+        path: './postcss.config.js',
+      },
+    }),
+  ],
+});
