76ef6e209f6b0e0dbf947f6d63872dbbfe239c05
Bishwa <bishwakiran725@gmail.com>
2024-01-13 21:33:57 +0545

    Updated Packages

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import postcss from 'rollup-plugin-postcss';
 
 export default defineConfig({
   plugins: [
     react(),
-    postcss({
-      config: {
-        path: './postcss.config.js',
-      },
-    }),
+    {
+      
+    },
   ],
 });
