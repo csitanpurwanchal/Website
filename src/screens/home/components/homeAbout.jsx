4f32a45d4d9cd5b33c2b0baeddaf8724406dcd9b
Bishwa <bishwakiran725@gmail.com>
2024-01-14 21:05:18 +0545

    Initialization

---
+++ src/screens/home/components/homeAbout.jsx
@@ -0,0 +1,13 @@
+import "./homeAbout.css";
+
+const HomeAbout = () => {
+  return (
+    <>
+      <div className="homeAbout">
+        <div className="content"></div>
+      </div>
+    </>
+  );
+};
+
+export default HomeAbout;
