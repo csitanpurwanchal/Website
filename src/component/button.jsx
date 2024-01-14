88e7ab5b09cd7f11866e77984bc5da307b86d79f
Bishwa <bishwakiran725@gmail.com>
2024-01-14 21:00:02 +0545

    added font Size parameter

--- src/component/button.jsx
+++ src/component/button.jsx
@@ -1,17 +1,29 @@
 // Button.js
-import React from 'react';
-import './button.css';
+import React from "react";
+import "./button.css";
 
-const Button = ({ text, onClick, color = 'CE4444', textColor = 'text-white' }) => {
+const CustomButton = ({
+  text,
+  onClick,
+  color = "CE4444",
+  textColor = "fff",
+  fontSize = 20,
+}) => {
   const buttonStyle = {
     backgroundColor: `#${color}`,
+    color: `#${textColor}`,
+    fontSize: fontSize,
   };
 
   return (
-    <button className={`button ${textColor}`} style={buttonStyle} onClick={onClick}>
+    <button
+      className={`button ${textColor}`}
+      style={buttonStyle}
+      onClick={onClick}
+    >
       {text}
     </button>
   );
 };
 
-export default Button;
+export default CustomButton;
