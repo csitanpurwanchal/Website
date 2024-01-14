55b8ef8b8f16f1606f0439b155307194db9f7682
Bishwa <bishwakiran725@gmail.com>
2024-01-14 21:03:43 +0545

    updated animation

--- src/component/noticesBar.jsx
+++ src/component/noticesBar.jsx
@@ -5,6 +5,15 @@ const noticesData = [
   { id: 1, heading: 'Important Notice 1' },
   { id: 2, heading: 'Important Notice 2' },
   { id: 3, heading: 'Important Notice 3' },
+  { id: 4, heading: 'Important Notice 1' },
+  { id: 5, heading: 'Important Notice 2' },
+  { id: 6, heading: 'Important Notice 3' },
+  { id: 7, heading: 'Important Notice 1' },
+  { id: 8, heading: 'Important Notice 2' },
+  { id: 9, heading: 'Important Notice 3' },
+  { id: 10, heading: 'Important Notice 1' },
+  { id: 11, heading: 'Important Notice 2' },
+  { id: 12, heading: 'Important Notice 3' },
   // Add more notices as needed
 ];
 
