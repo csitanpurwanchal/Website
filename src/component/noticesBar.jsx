10a4c6eec53aba1428171110b401d3ed992909b3
Bishwa <bishwakiran725@gmail.com>
2024-01-14 21:29:32 +0545

    Typo

--- src/component/noticesBar.jsx
+++ src/component/noticesBar.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import './noticesBar.css'; // Import the external CSS file
+import React from "react";
+import "./noticesbar.css"; // Import the external CSS file
 
 const noticesData = [
-  { id: 1, heading: 'Important Notice 1' },
-  { id: 2, heading: 'Important Notice 2' },
-  { id: 3, heading: 'Important Notice 3' },
-  { id: 4, heading: 'Important Notice 1' },
-  { id: 5, heading: 'Important Notice 2' },
-  { id: 6, heading: 'Important Notice 3' },
-  { id: 7, heading: 'Important Notice 1' },
-  { id: 8, heading: 'Important Notice 2' },
-  { id: 9, heading: 'Important Notice 3' },
-  { id: 10, heading: 'Important Notice 1' },
-  { id: 11, heading: 'Important Notice 2' },
-  { id: 12, heading: 'Important Notice 3' },
+  { id: 1, heading: "Important Notice 1" },
+  { id: 2, heading: "Important Notice 2" },
+  { id: 3, heading: "Important Notice 3" },
+  { id: 4, heading: "Important Notice 1" },
+  { id: 5, heading: "Important Notice 2" },
+  { id: 6, heading: "Important Notice 3" },
+  { id: 7, heading: "Important Notice 1" },
+  { id: 8, heading: "Important Notice 2" },
+  { id: 9, heading: "Important Notice 3" },
+  { id: 10, heading: "Important Notice 1" },
+  { id: 11, heading: "Important Notice 2" },
+  { id: 12, heading: "Important Notice 3" },
   // Add more notices as needed
 ];
 
