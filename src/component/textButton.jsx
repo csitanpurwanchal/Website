c559538d862a8d9cf6ccd0d140c4be0209996fb7
Bishwa <bishwakiran725@gmail.com>
2024-01-15 21:18:41 +0545

    Design changes

--- src/component/textButton.jsx
+++ src/component/textButton.jsx
@@ -1,4 +1,6 @@
+import React from "react";
 import { IoArrowForwardCircleOutline } from "react-icons/io5";
+// Import your stylesheet (create this file if not already present)
 
 const TextButton = ({
   text = "Know More",
@@ -7,20 +9,27 @@ const TextButton = ({
   fontSize = 20,
 }) => {
   const buttonStyle = {
+    display: "flex",
+    alignItems: "center", // Vertical alignment
     color: `#${color}`,
     fontSize: fontSize,
     padding: "10px",
+    cursor: "pointer", // Add pointer cursor
+    transition: "background-color 0.3s, border-radius 0.3s",
+    borderRadius: "5px", // Initial border-radius
   };
+
   const iconStyle = {
-    padding: "2px",
+    marginLeft: "8px",
+    marginTop: "3px", // Adjust the spacing between text and icon as needed
   };
 
   return (
     <>
-      <div className="textButton" style={buttonStyle}>
+      <div className="textButton" style={buttonStyle} onClick={onClick}>
         {text}
         <span style={iconStyle}>
-          <IoArrowForwardCircleOutline size={15} />
+          <IoArrowForwardCircleOutline size={20} />
         </span>
       </div>
     </>
